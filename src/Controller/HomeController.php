26570584fd6bc9fc595d1ceb9e911ec3ed880939
Cédric M <psychestrik@live.nl>
2023-03-30 15:02:03 +0200

    BDD + entity Ingredient

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,15 @@
 namespace App\Controller;
 
 
-class HomeController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class HomeController extends AbstractController
+{
+    #[Route('/', 'home.index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home.html.twig');
+    }
 }
