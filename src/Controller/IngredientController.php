f3b1014343a187cb28b6281aef7b93d1dcb70742
Cédric M <psychestrik@live.nl>
2023-05-12 18:28:45 +0200

    Gestion erreur liste ingredients vide

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -9,18 +9,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class IngredientController
+ * @package App\Controller
+ */
 class IngredientController extends AbstractController
 {
+    /**
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
     #[Route('/ingredient', name: 'ingredient')]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-
         $ingredients = $paginator->paginate(
             $repository->findAll(),
             $request->query->getInt('page', 1), /*page number*/
             10 /*limit per page*/
         );
-
 
         return $this->render('ingredient/index.html.twig', [
             'ingredients' => $ingredients
